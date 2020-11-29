c9ff2bfa54e613f19768c02e0cd96d04e29e8a37
Marc <marcjacobroy@gmail.com>
2020-11-28 19:25:18 -0500

    sanity check

--- src/main/java/Tuition/main/ServerDriver.java
+++ src/main/java/Tuition/main/ServerDriver.java
@@ -6,6 +6,9 @@ import org.apache.log4j.Logger;
 
 
 import Tuition.controller.EmployeeController;
+import Tuition.controller.RequestController;
+import Tuition.controller.EventController;
+
 
 // create a connection with javalin to process http requests on server with postman 
 // specify paths 
@@ -14,8 +17,12 @@ public class ServerDriver {
 	private static Logger log = Logger.getRootLogger();
 	
 	private static EmployeeController employeeController = new EmployeeController();
+	private static EventController eventController = new EventController();
+	private static RequestController requestController = new RequestController();
 	
 	private static final String EMPLOYEE_PATH = "/employee";
+	private static final String EVENT_PATH = "/event";
+	private static final String REQUEST_PATH = "/request";
 	
 	
 	
@@ -29,6 +36,15 @@ public class ServerDriver {
 		app.patch(EMPLOYEE_PATH, ctx -> employeeController.updateEmployee(ctx));
 		app.delete(EMPLOYEE_PATH, ctx -> employeeController.deleteEmployee(ctx));
 		
+		app.get(EVENT_PATH, ctx -> eventController.readEvent(ctx));
+		app.post(EVENT_PATH, ctx -> eventController.createEvent(ctx));
+		app.patch(EVENT_PATH, ctx -> eventController.updateEvent(ctx));
+		app.delete(EVENT_PATH, ctx -> eventController.deleteEvent(ctx));
+		
+		app.get(REQUEST_PATH, ctx -> requestController.readRequest(ctx));
+		app.post(REQUEST_PATH, ctx -> requestController.createRequest(ctx));
+		app.patch(REQUEST_PATH, ctx -> requestController.updateRequest(ctx));
+		app.delete(REQUEST_PATH, ctx -> requestController.deleteRequest(ctx));
 	}
 	
 }
