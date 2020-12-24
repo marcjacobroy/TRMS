8c1e333fa63daffc64031ca0be37713cbd9c6d96
Marc <marcjacobroy@gmail.com>
2020-12-24 12:07:38 -0500

    add env variables

--- src/main/java/Tuition/util/ConnectionUtil.java
+++ src/main/java/Tuition/util/ConnectionUtil.java
@@ -6,9 +6,13 @@ import java.sql.SQLException;
 // Connection factory
 public class ConnectionUtil {
 	
+	private static final String URL = System.getenv("TRMS_URL");
+	private static final String USERNAME = System.getenv("TRMS_USERNAME");
+	private static final String PASSWORD = System.getenv("TRMS_PASSWORD");
+	
 	public Connection createConnection() throws SQLException {
 		
-		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?", "postgres", "Pr1648%%");
+		Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 		
 		return connection;
 		
